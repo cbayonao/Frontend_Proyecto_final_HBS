a3f21ca079bf25df1d986b6007f8491ae072a588
MichaelAuditore <michael_v613@hotmail.com>
2020-06-03 14:29:21 -0500

    [Fix & Update] Separating Styles from the React View, creating Dynamic Content

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,132 +1,82 @@
 import React from 'react';
-import styled from 'styled-components';
+import {
+    ButtonLogin,
+    LoginStyle,
+    RInputStyle,
+    FormStyle, RDivInputStyle, RSelect,AlignCenter
+} from "../css/styles";
 
-const RegisterStyle = styled.div`
-    text-align: center;
-    background: #FFE5CC;
-    border-radius: 10px;
-    -webkit-box-shadow: 0px 10px 20px 10px rgba(0, 0, 0, 0.3);
-    padding: 2em 0 2em 0;
-    margin: 2em 0 4em 0;
-    width: 500px;
-`;
+class Register extends React.Component {
+    state = {
+        form: {
+            name: "",
+            lastname: "",
+            mobile: "",
+            email: "",
+            pwd: "",
+            cpwd: "",
+            type_id: "",
+            id: "",
+        }
+    }
 
-const RegisterContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-`;
+    handleChange = (e) => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            },
+        });
+    }
 
-const DivInputContainer = styled.div`
-    position: relative;
-    width: 99.5%;
-    background-color: #FFF7EF;
-    border: 1px solid #e6e6e6;
-    border-radius: 5px;
-    margin: 1em 0 1em 0;
-
-`;
-
-const InputRegister = styled.input`
-    height: 50px;
-    padding: 0 25px 0 25px;
-    border-radius: 5px;
-    font: 400 13.3333px Arial;
-    overflow: visible;
-    background: transparent;
-    outline: none;
-    border: none;
-    ::placeholder,
-    ::-webkit-input-placeholder {
-    font-style: italic;
-    font-weight: 300;
-    font-size: 15px;
-  }
-`;
-
-const FormStyle = styled.form`
-    padding: 0 1em 0 1em;
-`;
-
-const ButtonLogin = styled.button`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 20px;
-    width: 100%;
-    height: 60px;
-    background-color: #FFB266;
-    font-family: 'Roboto' sans-serif;
-    font-size: 14px;
-    color: #fff;
-    line-height: 1.2;
-    text-transform: uppercase;
-    text-align: center;
-    text-decoration: none;
-    transition: all 0.4s;
-    border: none;
-    overflow: visible;
-    border-radius: 5px;
-  }
-`;
-
-//Return implicito
-const Register = () => (
-    <RegisterStyle>
-        <div className="Register-container">
-            <div className="Register-logo">
-                <figure>
-                    <img src="/" alt="" />
-                </figure>
-            </div>
-            <div className="Register-titulo">
-                <h2>Registro</h2>
-            </div>
-            <FormStyle>
-                <RegisterContainer>
-                    <DivInputContainer>
-                        <InputRegister onChange="" type="text" name="name" id="InputName" placeholder="Ingresa tu Nombre" />
-                    </DivInputContainer>
-                    <DivInputContainer>
-                        <InputRegister onChange="" type="text" name="lastname" id="InputLastName" placeholder="Ingresa tu Apellido" />
-                    </DivInputContainer>
-                    <DivInputContainer>
-                        <InputRegister onChange="" type="tel" name="mobile" id="InputMobile" placeholder="Ingresa tu # de telefono" />
-                    </DivInputContainer>
-                    <DivInputContainer>
-                        <InputRegister onChange="" type="email" name="email" id="InputEmail" placeholder="Ingresa tu Email" />
-                    </DivInputContainer>
-                    <DivInputContainer>
-                        <InputRegister onChange="" type="password" name="password" id="InputPassword" placeholder="Ingresa tu Contraseña" />
-                    </DivInputContainer>
-                    <DivInputContainer>
-                        <InputRegister onChange="" type="password" name="password" id="InputPassword" placeholder="Verifica tu Contraseña" />
-                    </DivInputContainer>
-                    <DivInputContainer>
-                        <label for="cars">Tipo Documento</label>
-                        <br/>
-                        <select name="type-id" id="type-id">
-                            <option value="1">Cedula de ciudadania</option>
-                            <option value="2">Cedula de extranjeria</option>
-                            <option value="3">Pasaporte</option>
-                        </select>
-                    </DivInputContainer>
-                    <DivInputContainer>
-                        <InputRegister onChange="" type="text" name="id" id="InputId" placeholder="Ingresa tu # de identificacion" />
-                    </DivInputContainer>
-                </RegisterContainer>
-                    <div className="Register-input-checkbox">
-                        <input onChange="" type="checkbox" name="checkbox-login" id="Inputcheckbox" />
-                        <label for="Inputcheckbox">Recuerdame</label>
+    print = () => {
+        console.log(this.state);
+    }
+    //Return implicito
+    render() {
+        return (
+            <LoginStyle style={{ width: '555px' }}>
+                <div className="Register-container">
+                    <div className="Register-logo">
+                        <figure>
+                            <img src="/" alt="" />
+                        </figure>
                     </div>
-                    <div className="Register-button">
-                        <ButtonLogin type="submit" onClick="">Registrate</ButtonLogin>
+                    <div className="Register-titulo">
+                        <AlignCenter>Registro</AlignCenter>
                     </div>
-            </FormStyle>
-            <div className="Register-conditions">
-                <p>Al registrarte aceptas nuestras <a href="/"> condiciones</a></p>
-            </div>
-        </div>
-    </RegisterStyle>
-);
-
+                    <form>
+                        <RDivInputStyle>
+                            <RInputStyle onChange={this.handleChange} type="text" name="name" id="InputName" placeholder="Ingresa tu Nombre" />
+                            <RInputStyle onChange={this.handleChange} type="text" name="lastname" id="InputLastName" placeholder="Ingresa tu Apellido" />
+                        </RDivInputStyle>
+                        <RDivInputStyle>
+                            <RInputStyle onChange={this.handleChange} type="tel" name="mobile" id="InputMobile" placeholder="Ingresa tu # de telefono" />
+                            <RInputStyle onChange={this.handleChange} type="email" name="email" id="InputEmail" placeholder="Ingresa tu Email" />
+                        </RDivInputStyle>
+                        <RDivInputStyle>
+                            <RInputStyle onChange={this.handleChange} type="password" name="pwd" id="pwd" placeholder="Ingresa tu Contraseña" />
+                            <RInputStyle onChange={this.handleChange} type="password" name="cpwd" id="cpwd" placeholder="Verifica tu Contraseña" />
+                        </RDivInputStyle>
+                        <RDivInputStyle>
+                            <RSelect name="type_id" id="type_id" defaultValue={"#"} onChange={this.handleChange}>
+                                <option value="#" disabled>Tipo Documento</option>
+                                <option value="CC">Cedula de ciudadania</option>
+                                <option value="CE">Cedula de extranjeria</option>
+                                <option value="P">Pasaporte</option>
+                            </RSelect>
+                            <RInputStyle onChange={this.handleChange} type="text" name="id" id="InputId" placeholder="Ingresa tu # de identificacion" />
+                        </RDivInputStyle>
+                        <div className="Register-button">
+                            <ButtonLogin style={{ width: '80%', margin: '0 auto' }} type="button" onClick={this.print}>Registrate</ButtonLogin>
+                        </div>
+                    </form>
+                    <RDivInputStyle className="Register-conditions">
+                        <p style={{ margin: '2.5%' }}>Al registrarte aceptas nuestras <a href="/"> condiciones</a></p>
+                    </RDivInputStyle>
+                </div>
+            </LoginStyle>
+        );
+    }
+}
 export default Register;
