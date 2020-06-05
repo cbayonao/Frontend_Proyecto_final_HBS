9bf0616370c137af333d99ee461732a043d3efc1
MichaelAuditore <michael_v613@hotmail.com>
2020-06-04 20:57:46 -0500

    [Update] Register working successfully with cognito Email Verification added Redirection when login is success

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,118 +1,75 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { ButtonLogin } from '../css/login';
 import {
-    ButtonLogin,
-    LoginStyle,
-    RInputStyle,
-    FormStyle, RDivInputStyle, RSelect,AlignCenter
-} from "../css/styles";
+    RDivInputStyle, RSelect, RInputStyle
+} from "../css/register";
+import { FormStyle, LoginStyle, AlignCenter } from "../css/global";
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
-class Register extends React.Component {
-    state = {
-        form: {
-            name: "",
-            lastname: "",
-            mobile: "",
-            email: "",
-            pwd: "",
-            cpwd: "",
-            type_id: "",
-            id: "",
-        }
-    }
+export default () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    handleChange = (e) => {
-        this.setState({
-            form: {
-                ...this.state.form,
-                [e.target.name]: e.target.value
-            },
+    const poolData = {
+        UserPoolId: 'us-east-2_B6iWRQ83v',
+        ClientId: '7c6oic7nsjeijr1ck9dsgdak65'
+    };
+
+    const UserPool = new CognitoUserPool(poolData);
+
+    const onSubmit = event => {
+        event.preventDefault();
+        UserPool.signUp(email, password, [], null, (err, data) => {
+            if (err.code === "UsernameExistsException") { window.location.href = "#/"; }
+            else { window.location.href = "#/verification"; }
+
         });
-    }
+    };
 
-    print = () => {
-        console.log(this.state);
-    }
-    
-    render() {
-        return (
-            <LoginStyle style={{ width: '555px' }}>
-                <div className="Register-container">
-                    <div className="Register-logo">
-                        <figure>
-                            <img src="/" alt="" />
-                        </figure>
+    return (
+        <LoginStyle style={{ width: '555px' }}>
+            <div className="Register-container">
+                <div className="Register-logo">
+                    <figure>
+                        <img src="/" alt="" />
+                    </figure>
+                </div>
+                <div className="Register-titulo">
+                    <AlignCenter>Registro</AlignCenter>
+                </div>
+                <FormStyle onSubmit={onSubmit}>
+                    {/*  <RDivInputStyle>
+                        <RInputStyle type="text" name="name" id="InputName" placeholder="Ingresa tu Nombre" />
+                        <RInputStyle type="text" name="lastname" id="InputLastName" placeholder="Ingresa tu Apellido" />
+                    </RDivInputStyle> */}
+                    <RDivInputStyle>
+                        {/* <RInputStyle type="tel" name="mobile" id="InputMobile" placeholder="Ingresa tu # de telefono" /> */}
+                        <RInputStyle value={email} onChange={event => setEmail(event.target.value)} type="email" name="email" id="InputEmail" placeholder="Ingresa tu Email" />
+                        {/* </RDivInputStyle> */}
+                        {/* <RDivInputStyle> */}
+                        <RInputStyle value={password} onChange={event => setPassword(event.target.value)} type="password" name="pwd" id="pwd" placeholder="Ingresa tu Contraseña" />
+                        {/*     <RInputStyle type="password" name="cpwd" id="cpwd" placeholder="Verifica tu Contraseña" />
+                    </RDivInputStyle>
+                    <RDivInputStyle>
+                        <RSelect name="type_id" id="type_id" defaultValue={"#"}>
+                            <option value="#" disabled>Tipo Documento</option>
+                            <option value="CC">Cedula de ciudadania</option>
+                            <option value="CE">Cedula de extranjeria</option>
+                            <option value="P">Pasaporte</option>
+                        </RSelect>
+                        <RInputStyle type="text" name="id" id="InputId" placeholder="Ingresa tu # de identificacion" />
+                        */}
+                    </RDivInputStyle>
+
+                    <div className="Register-button">
+                        <ButtonLogin style={{ width: '80%', margin: '0 auto' }} type="submit">Registrate</ButtonLogin>
                     </div>
-                    <div className="Register-titulo">
-                        <AlignCenter>Registro</AlignCenter>
-                    </div>
-                    <form>
-                        <RDivInputStyle>
-                            <RInputStyle onChange={this.handleChange}
-                              type="text"
-                              name="name"
-                              id="InputName"
-                              placeholder="Ingresa tu Nombre" required />
-                            <RInputStyle onChange={this.handleChange}
-                              type="text"
-                              name="lastname"
-                              id="InputLastName"
-                              placeholder="Ingresa tu Apellido" required />
-                        </RDivInputStyle>
-                        <RDivInputStyle>
-                            <RInputStyle onChange={this.handleChange}
-                              type="tel"
-                              name="mobile"
-                              id="InputMobile"
-                              placeholder="Numero de telefono" required />
-                            <RInputStyle onChange={this.handleChange}
-                              type="email"
-                              name="email"
-                              id="InputEmail"
-                              placeholder="Ingresa tu Email" required />
-                        </RDivInputStyle>
-                        <RDivInputStyle>
-                            <RInputStyle onChange={this.handleChange}
-                              type="password"
-                              name="pwd"
-                              id="pwd"
-                              placeholder="Ingresa tu Contraseña" required />
-                            <RInputStyle onChange={this.handleChange}
-                              type="password"
-                              name="cpwd"
-                              id="cpwd"
-                              placeholder="Verifica tu Contraseña" required />
-                        </RDivInputStyle>
-                        <RDivInputStyle>
-                            <RSelect name="type_id"
-                              id="type_id"
-                              defaultValue={"#"}
-                              onChange={this.handleChange} required>
-                                <option value="#" disabled>Tipo Documento</option>
-                                <option value="CC">Cedula de ciudadania</option>
-                                <option value="CE">Cedula de extranjeria</option>
-                                <option value="P">Pasaporte</option>
-                            </RSelect>
-                            <RInputStyle
-                              onChange={this.handleChange}
-                              type="text"
-                              name="id"
-                              id="InputId"
-                              placeholder="Ingresa tu # de identificacion" />
-                        </RDivInputStyle>
-                        <div className="Register-button">
-                            <ButtonLogin style={{ width: '80%', margin: '0 auto' }}
-                              type="button"
-                              onClick={this.print}
-                              href="/#/verification">Registrate</ButtonLogin>
-                        </div>
-                    </form>
+
                     <RDivInputStyle className="Register-conditions">
                         <p style={{ margin: '2.5%' }}>Al registrarte aceptas nuestras <a href="/"> condiciones</a></p>
                     </RDivInputStyle>
-                </div>
-            </LoginStyle>
-        );
-    }
+                </FormStyle>
+            </div>
+        </LoginStyle>
+    );
 }
-export default Register;
