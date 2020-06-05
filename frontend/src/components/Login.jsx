f4028e4b50ac80bcc1afa477e7470f2fe81a6806
MichaelAuditore <michael_v613@hotmail.com>
2020-06-04 20:56:41 -0500

    [Update] New funcionality Validates with cognito if user exists, Login working successfully

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,86 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import {
     ButtonLogin,
     DivButtonLogin,
-    SpanStyle,
-    LoginStyle,
     LabelCheckBox,
     InputStyle,
-    InputCheckbox, FormStyle, DivInputStyle,
+    InputCheckbox, DivInputStyle,
     DivCheckbox
-} from "../css/styles";
+} from "../css/login";
+import { FormStyle, LoginStyle, AlignCenter } from "../css/global";
+import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import UserPool from "../UserPool";
+import Cookies from 'universal-cookie';
 
-class Login extends React.Component {
-    state = {
-        form: {
-            email: "",
-            pwd: "",
-        }
-    }
 
-    handleChange = (e) => {
-        this.setState({
-            form: {
-                ...this.state.form,
-                [e.target.name]: e.target.value
-            },
+export default () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const cookies = new Cookies();
+
+    const onSubmit = event => {
+        event.preventDefault();
+
+        const user = new CognitoUser({
+            Username: email,
+            Pool: UserPool
         });
-    }
+        const authDetails = new AuthenticationDetails({
+            Username: email,
+            Password: password
+        });
 
-    print = () => {
-        console.log(this.state);
-    }
+        user.authenticateUser(authDetails, {
+            onSuccess: data => {
+                console.log(data);
+                console.log(cookies.get('sub'));
+                console.log(cookies.get('token'));
+                window.location.href = "/#/home";
+            },
 
-    render() {
-        return (
-            <LoginStyle>
-                <div className="Login-container">
-                    <div className="Login-logo">
-                        <figure>
-                            <img src="/" alt="" />
-                        </figure>
-                    </div>
-                    <FormStyle>
-                        <div className="Login-titulo">
-                            <h1 style={{textAlign: "center"}}>Bienvenido</h1>
-                        </div>
-                        <DivInputStyle>
-                            <InputStyle onChange={this.handleChange}
-                              type="email"
-                              name="email"
-                              id="InputEmail"
-                              placeholder="Ingresa tu Email" />
-                        </DivInputStyle>
-                        <DivInputStyle>
-                            <InputStyle
-                              onChange={this.handleChange}
-                              type="password"
-                              name="pwd"
-                              id="InputPassword"
-                              pattern=".*"
-                              placeholder="Ingresa tu Contraseña" />
-                        </DivInputStyle>
-                        <DivCheckbox>
-                            <InputCheckbox
-                              type="checkbox"
-                              name="checkbox-login"
-                              id="Inputcheckbox" />
-                            <LabelCheckBox
-                              htmlFor="Inputcheckbox">Recuerdame</LabelCheckBox>
-                        </DivCheckbox>
-                        <DivButtonLogin>
-                            <ButtonLogin
-                              type="button"
-                              onClick={this.print}>Ingresa</ButtonLogin>
-                        </DivButtonLogin>
-                    </FormStyle>
-                    <div className="Login-Register" style={{margin: '5%'}}>
-                        <p>No estas registrado? <a href="/#/register"> crea tu cuenta</a></p>
-                    </div>
+            onFailure: err => {
+                if (err.code === "UserNotConfirmedException") {
+                    window.location.href = "/#/verification";
+                } else {
+                    $("#modal-close").css("display", "flex");
+                    $("#modal-close").css("transition-timing-function", "ease");
+                    setTimeout(() => {
+                        $("#modal-close").css("display", "none");
+                    }, 5000);
+                }
+            },
+
+            newPasswordRequired: data => {
+                console.log("newPasswordRequired:", data);
+            }
+        });
+    };
+
+
+    return (
+        <LoginStyle>
+            <div className="Login-container">
+                <div className="Login-logo">
+                    <figure>
+                        <img src="/" alt="" />
+                    </figure>
                 </div>
-            </LoginStyle>
-        );
-    }
+                <FormStyle onSubmit={onSubmit}>
+                    <div className="Login-titulo">
+                        <h1 style={{ textAlign: "center" }}>Bienvenido</h1>
+                    </div>
+                    <DivInputStyle>
+                        <InputStyle value={email} onChange={event => setEmail(event.target.value)} type="email" name="email" id="InputEmail" placeholder="Ingresa tu Email" />
+                    </DivInputStyle>
+                    <DivInputStyle>
+                        <InputStyle value={password} onChange={event => setPassword(event.target.value)} type="password" name="password" id="InputPassword" placeholder="Ingresa tu Contraseña" />
+                    </DivInputStyle>
+                    <DivCheckbox>
+                        <InputCheckbox type="checkbox" name="checkbox-login" id="Inputcheckbox" />
+                        <LabelCheckBox htmlFor="Inputcheckbox">Recuerdame</LabelCheckBox>
+                    </DivCheckbox>
+                    <DivButtonLogin>
+                        <ButtonLogin type="submit">Ingresa</ButtonLogin>
+                    </DivButtonLogin>
+                </FormStyle>
+                <div className="Login-Register" style={{ margin: '5%' }}>
+                    <p>No estas registrado? <a href="/#/register"> crea tu cuenta</a></p>
+                </div>
+            </div>
+        </LoginStyle>
+    );
 }
-
-export default Login;
