6b29b6026023b1f462839614e93c7c068bdfde4a
MichaelAuditore <michael_v613@hotmail.com>
2020-06-08 22:06:01 -0500

    [Updated] new function to check if user exists in DB

--- frontend/src/hooks/getApi.js
+++ frontend/src/hooks/getApi.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import UserPool from "../UserPool";
 
 const api = 'http://scrawlawapiv1-env.eba-jpcs83r5.us-east-1.elasticbeanstalk.com/api/';
 
@@ -10,25 +9,17 @@ export const useGetData = () => {
             .then(response => response.json())
             .then(data => setData(data))
     }, []);
-    getUser();
     return mydata;
 }
 
-export const getUser = () => {
-    const [mydata, setData] = useState([]);
-    const sub = UserPool.getCurrentUser().getUsername();
-    const token = UserPool.getCurrentUser().storage[
-        "CognitoIdentityServiceProvider.3rj5a30prom7dudokc1b5dog4l.b8adaf62-43ef-4df9-857b-1ee078e9a02c.idToken"
-    ];
+export async function getUser(session) {
     const miInit = {
         method: 'GET',
-        headers: { 'X-MyApp-Authorization': "Bearer " + token },
+        headers: { 'X-MyApp-Authorization': "Bearer " + session.idToken.jwtToken },
         cache: 'default'
     };
-    useEffect(() => {
-        fetch(api + 'users/' + sub, miInit)
-            .then(response => response)
-            .then(data => setData(data));
-    }, []);
-    return mydata;
+    return fetch(api + 'users/' + session.idToken.payload.sub, miInit)
+        .then(response => response)
+        .then(data => (data));
+
 }
