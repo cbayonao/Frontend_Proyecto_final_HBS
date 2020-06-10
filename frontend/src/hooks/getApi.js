28185f4de112c4566dfb518d08f9d8697ad2dc03
MichaelAuditore <michael_v613@hotmail.com>
2020-06-10 00:30:50 -0500

    [Updated] New Functions setRegister = create an user in DynamoDB searchProcess = look for a process and associate to the current user

--- frontend/src/hooks/getApi.js
+++ frontend/src/hooks/getApi.js
@@ -21,5 +21,38 @@ export async function getUser(session) {
     return fetch(api + 'users/' + session.idToken.payload.sub, miInit)
         .then(response => response)
         .then(data => (data));
+}
+
+
+export async function setRegister(json, sub, token) {
+    const miInit = {
+        method: 'POST',
+        headers: { 'X-MyApp-Authorization': "Bearer " + token, 'Content-Type': 'application/json' },
+        body: JSON.stringify(json),
+        cache: 'default'
+    };
+    return fetch(api + 'users/' + sub, miInit)
+        .then(response => {
+            if (response.ok) window.location.href = "#/home";
+            return false;
+        })
+}
+
+export async function searchProcess(process_id, sub, token) {
+    const miInit = {
+        method: 'POST',
+        headers: { 'X-MyApp-Authorization': "Bearer " + token },
+        cache: 'default'
+    };
+    $(".home").css("z-index", "-1000");
+    setTimeout(async () => {
+        return await fetch(api + 'processes/' + process_id + "/" + sub, miInit)
+            .then(response => {
+                if (response.ok) {
+                    window.location.href = "#/home";
+                }
+                return false;
+            })
+    });
 
 }
