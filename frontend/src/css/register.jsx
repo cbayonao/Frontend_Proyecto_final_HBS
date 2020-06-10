dcc5e77d3bcd0a081c16d1009f77ecac7416dba6
MichaelAuditore <michael_v613@hotmail.com>
2020-06-10 00:28:28 -0500

    [Updated] GlobalStyle to take media queries

--- frontend/src/css/register.jsx
+++ frontend/src/css/register.jsx
@@ -1,36 +1,44 @@
-import styled  from 'styled-components';
+import styled, { createGlobalStyle } from "styled-components";
 
 export const RInputStyle = styled.input`
-height: 50px;
-padding: 0 25px 0 25px;
-border-radius: 5px;
-font: 400 13.3333px Arial;
-overflow: visible;
-background: transparent;
-outline: none;
-border: 1px solid #e6e6e6;
-width: 100%;
-margin: 2%;
+  height: 50px;
+  padding: 0 25px 0 25px;
+  border-radius: 5px;
+  font: 400 13.3333px Arial;
+  overflow: visible;
+  background: transparent;
+  outline: none;
+  border: 1px solid #e6e6e6;
+  width: 100%;
+  margin: 2%;
+`;
+
+export const RDivResponsive = createGlobalStyle`
+    @media (max-width: 500px) {
+        .flex {
+            flex-flow: row wrap;
+        }
+    }
 `;
 
 export const RDivInputStyle = styled.div`
-display: flex;
-position: relative;
-width: 99.5%;
-background-color: #fff;
-border-radius: 5px;
-margin: 1em 0 1em 0;
+  display: flex;
+  position: relative;
+  width: 99.5%;
+  background-color: #fff;
+  border-radius: 5px;
+  margin: 1em 0 1em 0;
 `;
 export const RSelect = styled.select`
-height: 50px;
-padding: 0 25px 0 20px;
-border-radius: 5px;
-font: 400 13.3333px Arial;
-overflow: visible;
-background: transparent;
-outline: none;
-border: 1px solid #e6e6e6;
-width: 100%;
-margin: 2%;
-color: #767676;
+  height: 50px;
+  padding: 0 25px 0 20px;
+  border-radius: 5px;
+  font: 400 13.3333px Arial;
+  overflow: visible;
+  background: transparent;
+  outline: none;
+  border: 1px solid #e6e6e6;
+  width: 100%;
+  margin: 2%;
+  color: #767676;
 `;
