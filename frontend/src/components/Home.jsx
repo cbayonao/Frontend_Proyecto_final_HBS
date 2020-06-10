c163d6fad65b16df7cc580ac721d489e4ebe893f
MichaelAuditore <michael_v613@hotmail.com>
2020-06-10 00:24:33 -0500

    [Added] Search Function

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,37 +1,33 @@
 import React, { useEffect, useState, useContext } from "react";
-import { LoginStyle } from "../css/global";
+import { LoginStyle, WaitImg } from "../css/global";
 import { RDivInputStyle } from "../css/register";
 import {
   ButtonHomeInpProc,
   TitleHome,
-  Tr,
-  Td,
-  Tbody,
   HomeDivTable,
-  TableHome,
-  ButtonHomeBuscar,
-  ButtonHomeIntesity,
-  ButtonHomeDelete,
   InputProcHome,
   LabelInputHome,
 } from "../css/home";
 import { AccountContext } from "./Accounts";
+import ResumeProce from "../components/ResumeProce";
+import { getUser, searchProcess } from "../hooks/getApi";
 import UserPool from "../UserPool";
-import { getUser } from "../hooks/getApi";
 
 //Return implicito
 export default () => {
   const [status, setStatus] = useState(false);
+  const [numeroProceso, setNumero] = useState("");
 
   const { getSession, logout } = useContext(AccountContext);
 
   useEffect(() => {
-    const user = UserPool.getCurrentUser();
-    if (!user) {
+    if (!UserPool.getCurrentUser()) {
       window.location.href = "#/";
     }
     getSession().then((session) => {
       getUser(session).then((response) => {
+        console.log(session.idToken.jwtToken);
+        console.log(session.idToken.payload.sub);
         if (response.status == 400) {
           window.location.href = "#/fregister";
         }
@@ -47,10 +43,21 @@ export default () => {
     window.location.href = "#/";
   };
 
+  const addProcess = (event) => {
+    event.preventDefault;
+    getSession().then((session) => {
+      searchProcess(
+        numeroProceso,
+        session.idToken.payload.sub,
+        session.idToken.jwtToken
+      );
+    });
+  };
+
   return (
     <div>
       {status ? (
-        <LoginStyle style={{ width: "1000px", height: "500px" }}>
+        <LoginStyle className="home" style={{ width: "100%" }}>
           <RDivInputStyle>
             <TitleHome>Bienvenid@</TitleHome>
             <button type="button" onClick={signOut}>
@@ -60,43 +67,23 @@ export default () => {
           <LabelInputHome>
             Ingresa el numero del proceso (23 digitos):
           </LabelInputHome>
-          <InputProcHome type="text" placeholder="Numero de Proceso" required />
-          <ButtonHomeInpProc>Ingresar</ButtonHomeInpProc>
+          <InputProcHome
+            value={numeroProceso}
+            onChange={(event) => setNumero(event.target.value)}
+            type="text"
+            placeholder="Numero de Proceso"
+            required
+          />
+          <ButtonHomeInpProc onClick={addProcess}>Ingresar</ButtonHomeInpProc>
           <HomeDivTable>
-            <TableHome>
-              <thead>
-                <Tr>
-                  <th>Numero del proceso</th>
-                  <th>Partes del Proceso</th>
-                  <th>Opciones</th>
-                </Tr>
-              </thead>
-              <Tbody>
-                <Tr>
-                  <Td>
-                    <a href="/#/processdetail">
-                      <div>11001310502620140058300</div>
-                    </a>
-                  </Td>
-                  <Td>Demandante: XXXXXX - Demandado: XXXXX</Td>
-                  <Td>
-                    <ButtonHomeBuscar className="Buscar">
-                      Buscar
-                    </ButtonHomeBuscar>
-                    <ButtonHomeIntesity className="Intensidad">
-                      Intensidad
-                    </ButtonHomeIntesity>
-                    <ButtonHomeDelete className="Eliminar">
-                      Eliminar
-                    </ButtonHomeDelete>
-                  </Td>
-                </Tr>
-              </Tbody>
-            </TableHome>
+            <ResumeProce />
+            <ResumeProce />
+            <ResumeProce />
+            <ResumeProce />
           </HomeDivTable>
         </LoginStyle>
       ) : (
-        "No hay nada"
+        ""
       )}
     </div>
   );
