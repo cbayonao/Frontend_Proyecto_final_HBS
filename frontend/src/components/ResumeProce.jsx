c63c83f3222feb7685f9ae6634b0308998c0353b
MichaelAuditore <michael_v613@hotmail.com>
2020-06-10 00:29:31 -0500

    [Updated] Paint each process as a Table

--- frontend/src/components/ResumeProce.jsx
+++ frontend/src/components/ResumeProce.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React from "react";
+import { Process, Buttons, Button } from "../css/process";
+import { Responsive } from "../css/process";
 
 //Return implicito
-const ResumeProce = () => (
-    <h1>Este es el ResumeProce</h1>
-);
-
-export default ResumeProce;
+export default () => {
+  return (
+    <Process>
+      <Responsive />
+      <table>
+        <thead>
+          <tr>
+            <th>Numero de Proceso</th>
+            <th>Demandante</th>
+            <th>Demandado</th>
+            <th>Fecha de Radicacion</th>
+            <th>Tipo de Proceso</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>215216141242</td>
+            <td>Juan Lopez</td>
+            <td>Sergio Garcias</td>
+            <td>25-01-2020</td>
+            <td>Declarativo</td>
+          </tr>
+        </tbody>
+      </table>
+      <Buttons className="buttons">
+        <Button>Ver</Button>
+        <Button>Intensidad de Proceso</Button>
+        <Button>Eliminar</Button>
+      </Buttons>
+    </Process>
+  );
+};
