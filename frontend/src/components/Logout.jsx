4f06df9fe374716ece629541aab05620c6f33c56
MichaelAuditore <michael_v613@hotmail.com>
2020-06-11 08:29:00 -0500

    [New component] Logout or Signout

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,8 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ListItemText from "@material-ui/core/ListItemText";
+import { getUser } from "../hooks/getApi";
+import UserPool from "../UserPool";
+import { AccountContext } from "./Accounts";
 
-//Return implicito
-const Logout = () => (
-    <h1>Este es el Logout</h1>
-);
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5",
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "center",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center",
+    }}
+    {...props}
+  />
+));
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    "&:focus": {
+      backgroundColor: theme.palette.primary.main,
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
 
-export default Logout;
+export default function CustomizedMenus() {
+  const { getSession, logout } = useContext(AccountContext);
+  useEffect(() => {
+    if (!UserPool.getCurrentUser()) {
+      window.location.href = "#/";
+    }
+    getSession().then((session) => {
+      getUser(session).then((response) => {
+        console.log(session.idToken.jwtToken);
+        console.log(session.idToken.payload.sub);
+        if (response.status == 400) {
+          window.location.href = "#/fregister";
+        }
+      });
+    });
+  }, []);
+  const signOut = (event) => {
+    event.preventDefault;
+    logout();
+    window.location.href = "#/";
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div>
+      <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="default"
+        onClick={handleClick}
+      >
+        <AccountCircleIcon style={{ fontSize: 30 }} />
+      </Button>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <SettingsIcon></SettingsIcon>
+          <Link to="/fregister">
+            <ListItemText primary="Configurar cuenta" />
+          </Link>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ExitToAppIcon></ExitToAppIcon>
+          <ListItemText onClick={signOut} primary="Cerrar sesion" />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
+  );
+}
---
+++ frontend/src/css/logout.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const DivProfileImg = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  margin: 1em 0 1em 0;
+`;
