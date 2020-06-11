778022da051e2202606d67708a10950d7856219e
MichaelAuditore <michael_v613@hotmail.com>
2020-06-11 08:29:25 -0500

    [Updated] New function, props to get data and generate dynamic elements

--- frontend/src/components/ResumeProce.jsx
+++ frontend/src/components/ResumeProce.jsx
@@ -4,74 +4,65 @@ import { Responsive } from "../css/process";
 import { getProcesses } from "../hooks/getApi";
 import { AccountContext } from "./Accounts";
 
-export default () => {
+export function PaintProcess(props) {
   const [processes, setProcesses] = useState([]);
   const { getSession } = useContext(AccountContext);
-  const [processId, setProcessId] = useState("");
-  useState(async () => {
+
+  if (props.render) {
     getSession().then((session) => {
       getProcesses(session).then((response) => {
         const proc = Object.keys(response).map((process) => {
-          console.log(response[process]);
           return [
             process,
             response[process]["parties"]["Demandante"],
             response[process]["parties"]["Demandado"],
             response[process]["radicated_at"],
             response[process]["type_proc"],
+            session,
           ];
         });
         setProcesses(proc);
       });
     });
-  });
-
-  const see = (event) => {
-    event.preventDefault;
-    console.log(processId);
-  };
-  const change = (event) => {
-    event.preventDefault;
-    console.log(processId);
-  };
-  const del = (event) => {
-    event.preventDefault;
-    console.log(processId);
-  };
-  return processes.map((process) => {
-    return (
-      <Process key={process}>
-        <Responsive />
-        <table>
-          <thead>
-            <tr>
-              <th>Numero de Proceso</th>
-              <th>Demandante</th>
-              <th>Demandado</th>
-              <th>Fecha de Radicacion</th>
-              <th>Tipo de Proceso</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{process[0]}</td>
-              <td>{process[1]}</td>
-              <td>{process[2]}</td>
-              <td>{process[3]}</td>
-              <td>{process[4]}</td>
-            </tr>
-          </tbody>
-        </table>
-        <Buttons className="buttons">
-          <Button onClick={(event) => setProcessId(process[0])}>Ver</Button>
-          <Button onClick={(event) => setProcessId(process[0])}>
-            Intensidad de Proceso
-          </Button>
-          <Button onClick={(event) => setProcessId(process[0])}>
-            Eliminar
-          </Button>
-        </Buttons>
-      </Process>
-    );
-  });
-};
+    return processes.map((process) => {
+      return (
+        <Process key={process} className={process[0]}>
+          <Responsive />
+          <table>
+            <thead>
+              <tr>
+                <th>Numero de Proceso</th>
+                <th>Demandante</th>
+                <th>Demandado</th>
+                <th>Fecha de Radicacion</th>
+                <th>Tipo de Proceso</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{process[0]}</td>
+                <td>{process[1]}</td>
+                <td>{process[2]}</td>
+                <td>{process[3]}</td>
+                <td>{process[4]}</td>
+              </tr>
+            </tbody>
+          </table>
+          <Buttons className="buttons">
+            <Button onClick={(props.setProcess(process[0]), props.view)}>
+              Ver
+            </Button>
+            <Button onClick={(props.setProcess(process[0]), props.change)}>
+              Intensidad de Proceso
+            </Button>
+            <Button onClick={(props.setProcess(process[0]), props.delete)}>
+              Eliminar
+            </Button>
+          </Buttons>
+        </Process>
+      );
+    });
+  } else {
+    return null;
+  }
+}
